d5ca5f0d4b44a752eb1b096f86f204cefec18120
Karooo000 <56841506+Karooo000@users.noreply.github.com>
2024-11-26 15:58:54 -0500

    first shipping

--- src/Preloader.jsx
+++ src/Preloader.jsx
@@ -1,11 +1,42 @@
+import { useRef } from "react";
+import { useProgress } from "@react-three/drei";
 import gsap from "gsap";
 
+
+const preLoader = document.querySelector(".contain-preloader")
+
 export default function Preloader() {
+
+  const { progress } = useProgress();
+  const loadingGone = useRef()
 
     let tl = gsap.timeline({ repeat: -1 });
 
        tl.to(".dot-first", { opacity: 1, duration: 0.5})
          .to(".dot-second", { opacity: 1, duration: 0.5 })
          .to(".dot-third", { opacity: 1, duration: 0.5 });
+
+    
+  setTimeout(() => {
+    if(progress > 99){
+      //preLoader.classList.remove("active")
+      console.log("code ran")
+      
+      
+
+      loadingGone.current = gsap.timeline()
+
+      loadingGone.current
+      .to(preLoader, {yPercent: -170, opacity: 0, duration: 1},"sameTimeeee")
+      
+      
+
+      setTimeout(() => {
+          preLoader.classList.remove("active")
+      }, "1200")
+      
+      
+    }
+}, "1200")
 
 }
