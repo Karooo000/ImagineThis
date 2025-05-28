f4fbf7548490790073078bdcaf468bdc6a581bba
Karooo000 <karolinbraun7@gmail.com>
2025-05-28 11:45:05 -0500

    Cleanup from innitial clone

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from "/src/App.jsx"
-import Config from './Config'
+
 
 
 createRoot(document.getElementById('root')).render(<App />)
-createRoot(document.getElementById('configg')).render(<Config />)
