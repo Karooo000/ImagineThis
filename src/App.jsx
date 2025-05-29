235b9d626e3305344ecb1402abdd694c3c98dc00
Karooo000 <karolinbraun7@gmail.com>
2025-05-29 12:31:15 -0500

    trying to get alpha working, saving in between

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import React, { useState, useEffect, useRef, Suspense } from "react";
 
 import {useProgress, Environment} from "@react-three/drei";
+import { EffectComposer, Bloom, HueSaturation, BrightnessContrast } from '@react-three/postprocessing';
+
+import Model from "./NeuralFractal.jsx"
 
 
+function CameraLayerSetup() {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    // Enable default layer 0 and bloom layer 1
+    camera.layers.enable(0);
+    camera.layers.enable(1);
+  }, [camera]);
+
+  return null;
+}
 
 
 function App() {
 
 //https://glassescode.netlify.app/hospital_room_2_1k.hdr
 
+//<ambientLight intensity={0.1}/>
+
+
+
 
   return (
     <>
      <Canvas shadows>
-        <Environment files='src/assets/hospital_room_2_1k.hdr' environmentIntensity={0.1}  />
-        <ambientLight intensity={0.15}/>
+        <Environment files='src/assets/hospital_room_2_1k.hdr' environmentIntensity={0.15}  />
+
+        <CameraLayerSetup />
+        
         <Suspense fallback={null}>
-         <group>
-           <mesh >
-              <boxGeometry args={[1, 1, 1]} />
-              
-              <meshStandardMaterial
-        color="#00aaff"
-        metalness={0.5}
-        roughness={0.3}
-      />
-            </mesh>
-          </group>
+        <group>
+          <Model/>
+        </group>
        </Suspense>
+
+       <EffectComposer multisampling={4}>
+          <Bloom
+            mipmapBlur
+            intensity={1.75}
+            luminanceThreshold={0.3} // Lower so emissive mesh glows
+            luminanceSmoothing={0.99}
+            height={300}
+            radius={0.45}
+          />
+          <HueSaturation saturation={0.45} />{/* increase saturation (0 default, positive >1 higher) */}
+          <BrightnessContrast
+            brightness={0.15} // brightness. min: -1, max: 1
+            contrast={0.35} // contrast: min -1, max: 1
+          />
+          
+        </EffectComposer>
       </Canvas>
     </>
 
---
+++ src/NeuralFractal.jsx
@@ -0,0 +1,90 @@
+import React, { useRef, useEffect } from 'react'
+import { useGLTF, PerspectiveCamera, useTexture } from '@react-three/drei'
+
+const modelURL = '/Fractals4.glb'
+
+export default function Model(props) {
+
+
+  const { nodes, materials } = useGLTF(modelURL)
+  const glowingRef = useRef()
+  const cameraRef = useRef()
+
+  const alphaMap = useTexture('src/assets/AlphaNew.jpg')
+
+  // Set glowing mesh to layer 1
+  useEffect(() => {
+    if (glowingRef.current) {
+      glowingRef.current.layers.set(1)
+    }
+  }, [])
+
+  //console.log(Object.entries(materials).flatMap(([k, v]) => Object.entries(v).filter(([key, val]) => val?.isTexture)))
+
+
+  return (
+    <group {...props} dispose={null}>
+      <pointLight
+        intensity={0.0154351413}
+        decay={2}
+        position={[4.076, 5.904, -1.005]}
+        rotation={[-1.839, 0.602, 1.932]}
+      />
+
+      {/* Expose camera ref to enable layers in App.jsx */}
+      <PerspectiveCamera
+        ref={cameraRef}
+        makeDefault={true}
+        far={100}
+        near={0.1}
+        fov={22.895}
+        position={[1.985, 1.473, -1.914]}
+        rotation={[-2.79, 0.1, 3.105]}
+      />
+
+      {/* Your main fractal mesh on default layer 0 */}
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.NeuralFractal.geometry}
+        //material={materials.NeuralMaterial}
+        position={[1.262, 0.976, -0.247]}
+        scale={0.01}
+      >
+           <meshStandardMaterial
+                map={materials.NeuralMaterial.map}
+                normalMap={materials.NeuralMaterial.normalMap}
+                roughnessMap={materials.NeuralMaterial.roughnessMap}
+                metalnessMap={materials.NeuralMaterial.metalnessMap}
+                alphaMap={alphaMap}
+                transparent={true}
+                ior={1.5} 
+                depthWrite={true}
+                depthTest={true}
+                side={2} // DoubleSide
+                //alphaTest={0.01} // Optional: removes fully transparent pixels
+            />
+      </mesh>
+
+      {/* Glowing mesh on layer 1 */}
+      <mesh
+        ref={glowingRef}
+        castShadow
+        receiveShadow
+        geometry={nodes.Spheres.geometry}
+        position={[1.262, 0.976, -0.247]}
+        scale={0.01}
+      >
+        <meshStandardMaterial
+          color="#34ebe8"
+          emissive="#34ebe8"
+          emissiveIntensity={1.5}
+        />
+      </mesh>
+    </group>
+
+    
+  )
+}
+
+useGLTF.preload(modelURL)
